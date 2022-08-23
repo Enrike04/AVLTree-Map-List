567a4ca85687ed18f42f8d760938b462b768b9bc
cyr <bochkarev.cyril@gmail.com>
2022-08-23 20:29:45 +0200

    mutable tests

--- src/test/kotlin/org/jub/kotlin/hometask3/MutableAvlTreeMapTest.kt
+++ src/test/kotlin/org/jub/kotlin/hometask3/MutableAvlTreeMapTest.kt
@@ -1,21 +1,50 @@
 package org.jub.kotlin.hometask3
 
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.RepeatedTest
+import kotlin.test.assertEquals
+import kotlin.test.assertNull
+import kotlin.test.assertTrue
 
 class MutableAvlTreeMapTest {
-    @Test
+
+    @RepeatedTest(testIterations)
     fun clear() {
+        val values = getSetOfRandomValues()
+        val avl: MutableAvlTreeMap<Int, String> = AvlTreeMapImpl(values.zip(values.map { it.toString() }))
+        avl.clear()
+        assertTrue(avl.isEmpty())
+        assertTrue(avl.size == 0)
     }
 
-    @Test
+    @RepeatedTest(testIterations)
     fun remove() {
+        val values = getSetOfRandomValues()
+        val avl: MutableAvlTreeMap<Int, String> = AvlTreeMapImpl(values.zip(values.map { it.toString() }))
+        var expectedSize = values.size
+        values.forEach {
+            avl.remove(it)
+            expectedSize -= 1
+            assertEquals(expectedSize, avl.size)
+        }
+        assertTrue(avl.isEmpty())
     }
 
-    @Test
-    fun putAll() {
-    }
-
-    @Test
+    @RepeatedTest(testIterations)
     fun put() {
+        val values = getSetOfRandomValues()
+        val avl: MutableAvlTreeMap<Int, String> = AvlTreeMapImpl()
+        assertTrue(avl.isEmpty())
+        var expectedSize = 0
+        values.forEach {
+            val old = avl.put(it, it.toString())
+            expectedSize += 1
+            assertEquals(expectedSize, avl.size)
+            assertNull(old)
+        }
+        values.forEach {
+            val old = avl.put(it, it.toString())
+            expectedSize += 1
+            assertEquals(it.toString(), old)
+        }
     }
 }
