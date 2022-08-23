e48fb804c7334a3bf55af8b7baf434cd76f4e954
cyr <bochkarev.cyril@gmail.com>
2022-08-23 20:12:56 +0200

    Use RepeatedTest annotation

--- src/test/kotlin/org/jub/kotlin/hometask3/AvlTreeMapTest.kt
+++ src/test/kotlin/org/jub/kotlin/hometask3/AvlTreeMapTest.kt
@@ -1,25 +1,30 @@
 package org.jub.kotlin.hometask3
 
+import org.junit.jupiter.api.RepeatedTest
 import kotlin.random.Random
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
 internal class AvlTreeMapTest {
-    private val testIterations = 10
-    private val testSetSize = 16
+    companion object {
+        private const val testIterations = 10
+        private const val testSetSize = 16
+    }
+
     private fun getSetOfRandomValues() = IntArray(testSetSize) { Random.nextInt() }.toSet()
 
     @Test
-    fun getSize() {
+    fun getSizeEmpty() {
         val emptyAvl: AvlTreeMap<Int, String> = BremenAvl()
         assertEquals(0, emptyAvl.size, "Default size should be 0")
+    }
 
-        for (i in 1..testIterations) {
-            val values = getSetOfRandomValues()
-            val avl: AvlTreeMap<Int, String> = BremenAvl(values.zip(values.map { it.toString() }))
-            assertEquals(values.size, avl.size, "Values and avl size should be equal")
-        }
+    @RepeatedTest(testIterations)
+    fun getSize() {
+        val values = getSetOfRandomValues()
+        val avl: AvlTreeMap<Int, String> = BremenAvl(values.zip(values.map { it.toString() }))
+        assertEquals(values.size, avl.size, "Values and avl size should be equal")
     }
 
     @Test
@@ -28,93 +33,77 @@ internal class AvlTreeMapTest {
         assertTrue(emptyAvl.isEmpty(), "Default tree should be empty")
     }
 
-    @Test
+    @RepeatedTest(testIterations)
     fun maximumKey() {
-        for (i in 1..testIterations) {
-            val values = getSetOfRandomValues()
-            val avl: AvlTreeMap<Int, Double> = BremenAvl(values.zip(values.map { it.toDouble() }))
-            assertEquals(values.max(), avl.maximumKey(), "Wrong max key")
-        }
+        val values = getSetOfRandomValues()
+        val avl: AvlTreeMap<Int, Double> = BremenAvl(values.zip(values.map { it.toDouble() }))
+        assertEquals(values.max(), avl.maximumKey(), "Wrong max key")
     }
 
-    @Test
+    @RepeatedTest(testIterations)
     fun minimumKey() {
-        for (i in 1..testIterations) {
-            val values = getSetOfRandomValues()
-            val avl: AvlTreeMap<Int, Double> = BremenAvl(values.zip(values.map { it.toDouble() }))
-            assertEquals(values.min(), avl.minimumKey(), "Wrong min key")
-        }
+        val values = getSetOfRandomValues()
+        val avl: AvlTreeMap<Int, Double> = BremenAvl(values.zip(values.map { it.toDouble() }))
+        assertEquals(values.min(), avl.minimumKey(), "Wrong min key")
     }
 
-    @Test
+    @RepeatedTest(testIterations)
     fun maximumValue() {
-        for (i in 1..testIterations) {
-            val values = getSetOfRandomValues()
-            val avl: AvlTreeMap<Int, Double> = BremenAvl(values.zip(values.map { it.toDouble() }))
-            assertEquals(values.max().toDouble(), avl.maximumValue(), "Wrong max value")
-        }
+        val values = getSetOfRandomValues()
+        val avl: AvlTreeMap<Int, Double> = BremenAvl(values.zip(values.map { it.toDouble() }))
+        assertEquals(values.max().toDouble(), avl.maximumValue(), "Wrong max value")
     }
 
-    @Test
+    @RepeatedTest(testIterations)
     fun minimumValue() {
-        for (i in 1..testIterations) {
-            val values = getSetOfRandomValues()
-            val avl: AvlTreeMap<Int, Double> = BremenAvl(values.zip(values.map { it.toDouble() }))
-            assertEquals(values.min().toDouble(), avl.minimumValue(), "Wrong min value")
-        }
+        val values = getSetOfRandomValues()
+        val avl: AvlTreeMap<Int, Double> = BremenAvl(values.zip(values.map { it.toDouble() }))
+        assertEquals(values.min().toDouble(), avl.minimumValue(), "Wrong min value")
     }
 
-    @Test
+    @RepeatedTest(testIterations)
     fun getEntries() {
-        for (i in 1..testIterations) {
-            val values = getSetOfRandomValues()
-            val doubleValues = values.map { it.toDouble() }.toSet()
-            val avl: AvlTreeMap<Int, Double> = BremenAvl(values.zip(doubleValues))
-            val entries = avl.entries
-            assertTrue(entries.all { it.key in values && it.value in doubleValues})
-            assertTrue(values.all { it in entries.map { it.key }})
-            assertTrue(doubleValues.all { it in entries.map { it.value }})
-        }
+        val values = getSetOfRandomValues()
+        val doubleValues = values.map { it.toDouble() }.toSet()
+        val avl: AvlTreeMap<Int, Double> = BremenAvl(values.zip(doubleValues))
+        val entries = avl.entries
+        assertTrue(entries.all { it.key in values && it.value in doubleValues })
+        assertTrue(values.all { it in entries.map { it.key } })
+        assertTrue(doubleValues.all { it in entries.map { it.value } })
     }
 
-    @Test
+    @RepeatedTest(testIterations)
     fun containsKey() {
-        for (i in 1..testIterations) {
-            val values = getSetOfRandomValues()
-            val avl: AvlTreeMap<Int, Double> = BremenAvl(values.zip(values.map { it.toDouble() }))
-            assertTrue(values.all { avl.containsKey(it) })
-            for (j in 1..testSetSize) {
-                val testVal = Random.nextInt()
-                assertTrue(testVal in values || !avl.containsKey(testVal))
-            }
+        val values = getSetOfRandomValues()
+        val avl: AvlTreeMap<Int, Double> = BremenAvl(values.zip(values.map { it.toDouble() }))
+        assertTrue(values.all { avl.containsKey(it) })
+        for (j in 1..testSetSize) {
+            val testVal = Random.nextInt()
+            assertTrue(testVal in values || !avl.containsKey(testVal))
         }
     }
 
-    @Test
+    @RepeatedTest(testIterations)
     fun containsValue() {
-        for (i in 1..testIterations) {
-            val values = getSetOfRandomValues()
-            val avl: AvlTreeMap<Int, Double> = BremenAvl(values.zip(values.map { it.toDouble() }))
-            assertTrue(values.all { avl.containsValue(it.toDouble()) })
-            for (j in 1..testSetSize) {
-                val testVal = Random.nextInt()
-                assertTrue(testVal in values || !avl.containsValue(testVal.toDouble()))
-            }
+        val values = getSetOfRandomValues()
+        val avl: AvlTreeMap<Int, Double> = BremenAvl(values.zip(values.map { it.toDouble() }))
+        assertTrue(values.all { avl.containsValue(it.toDouble()) })
+        for (j in 1..testSetSize) {
+            val testVal = Random.nextInt()
+            assertTrue(testVal in values || !avl.containsValue(testVal.toDouble()))
         }
     }
 
-    @Test
+    @RepeatedTest(testIterations)
     fun get() {
-        for (i in 1..testIterations) {
-            val values = getSetOfRandomValues()
-            val avl: AvlTreeMap<Int, Double> = BremenAvl(values.zip(values.map { it.toDouble() }))
-            values.forEach {
-                assertEquals(it.toDouble(), avl[it])
-            }
-            for (j in 1..testSetSize) {
-                val testVal = Random.nextInt()
-                assertTrue(testVal in values || avl[testVal] == null)
-            }
+        val values = getSetOfRandomValues()
+        val avl: AvlTreeMap<Int, Double> = BremenAvl(values.zip(values.map { it.toDouble() }))
+        values.forEach {
+            assertEquals(it.toDouble(), avl[it])
+        }
+        for (j in 1..testSetSize) {
+            val testVal = Random.nextInt()
+            assertTrue(testVal in values || avl[testVal] == null)
         }
     }
 }
