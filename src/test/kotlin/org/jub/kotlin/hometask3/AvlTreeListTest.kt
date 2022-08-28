33d6bb2d0a6584401e6ac094e1b03243f4f1a19a
cyr <bochkarev.cyril@gmail.com>
2022-08-28 14:06:53 +0200

    more list tests

--- src/test/kotlin/org/jub/kotlin/hometask3/AvlTreeListTest.kt
+++ src/test/kotlin/org/jub/kotlin/hometask3/AvlTreeListTest.kt
@@ -1,12 +1,16 @@
 package org.jub.kotlin.hometask3
 
+import org.junit.jupiter.api.RepeatedTest
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import kotlin.test.assertEquals
 import kotlin.test.assertFalse
+import kotlin.test.assertNotEquals
 import kotlin.test.assertTrue
 
 internal class AvlTreeListTest {
 
-    @Test
+    @RepeatedTest(testIterations)
     fun contains() {
         val values = getSetOfRandomValues()
         val avl: AvlTreeList<Int, Double> = AvlTreeImpl(values.zip(values.map { it.toDouble() }))
@@ -19,35 +23,94 @@ internal class AvlTreeListTest {
         }
     }
 
-    @Test
+    @RepeatedTest(testIterations)
     fun containsAll() {
+        val values = getSetOfRandomValues().toList()
+        val doubleValues = values.map { it.toDouble() }
+        val avl: AvlTreeList<Int, Double> = AvlTreeImpl(values.zip(doubleValues))
+        assertTrue(avl.containsAll(doubleValues + doubleValues))
+        val otherValues = getSetOfRandomValues()
+        otherValues.filter { it !in values }.forEach {
+            assertFalse(avl.containsAll(doubleValues + it.toDouble() + doubleValues))
+        }
     }
 
-    @Test
+    @RepeatedTest(testIterations)
     fun get() {
+        val values = getSetOfRandomValues().toList()
+        val doubleValues = values.map { it.toDouble() }
+        val avl: AvlTreeList<Int, Double> = AvlTreeImpl(values.zip(doubleValues))
+        values.sorted().forEachIndexed { index, value ->
+            assertEquals(value.toDouble(), avl[index])
+        }
     }
 
     @Test
-    operator fun iterator() {
+    fun getThrows() {
+        val values = getSetOfRandomValues()
+        val avl: AvlTreeList<Int, Double> = AvlTreeImpl(values.zip(values.map { it.toDouble() }))
+        for (i in 0..testSetSize) {
+            assertThrows<IndexOutOfBoundsException> {
+                avl[avl.size + i]
+            }
+        }
+        for (i in -1 downTo -avl.size) {
+            assertThrows<IllegalArgumentException> {
+                avl[i]
+            }
+        }
     }
 
-    @Test
+    @RepeatedTest(testIterations)
     fun listIterator() {
+        val values = getSetOfRandomValues().toList()
+        val avl: AvlTreeList<Int, Double> = AvlTreeImpl(values.zip(values.map { it.toDouble() }))
+        val valuesIterator = values.listIterator()
+        val avlIterator = avl.listIterator()
+        for (i in 1..values.size) {
+            assertTrue(avlIterator.hasNext())
+            assertEquals(valuesIterator.nextIndex(), avlIterator.nextIndex())
+            assertEquals(valuesIterator.next().toDouble(), avlIterator.next())
+        }
+        for (i in values.size downTo values.size.div(2)) {
+            assertTrue(avlIterator.hasPrevious())
+            assertEquals(valuesIterator.previousIndex(), avlIterator.previousIndex())
+            assertEquals(valuesIterator.previous().toDouble(), avlIterator.previous())
+        }
+        for (i in values.size.div(2)..values.size.div(4)) {
+            assertTrue(avlIterator.hasNext())
+            assertEquals(valuesIterator.nextIndex(), avlIterator.nextIndex())
+            assertEquals(valuesIterator.next().toDouble(), avlIterator.next())
+        }
     }
 
-    @Test
-    fun testListIterator() {
-    }
+//    @Test
+//    fun subList() {
+//    }
 
-    @Test
-    fun subList() {
-    }
-
-    @Test
+    @RepeatedTest(testIterations)
     fun lastIndexOf() {
+
     }
 
-    @Test
+    @RepeatedTest(testIterations)
     fun indexOf() {
+        val values = getSetOfRandomValues().toList()
+        val entries = values.flatMap { value -> List(3) { index -> "$value #$index" to value } }.sortedBy { it.first }
+        val avl: AvlTreeList<String, Int> = AvlTreeImpl(entries)
+        val encountered = mutableSetOf<Int>()
+        entries.forEachIndexed { index, (_, value) ->
+            if (value !in encountered) {
+                encountered.add(value)
+                assertEquals(index, avl.indexOf(value))
+            } else {
+                assertNotEquals(index, avl.indexOf(value))
+            }
+        }
+    }
+
+    @RepeatedTest(testIterations)
+    fun lastIndexVsIndex() {
+
     }
 }
