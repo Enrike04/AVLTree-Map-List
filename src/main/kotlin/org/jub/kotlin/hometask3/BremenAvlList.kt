9357271bc0e908d6f6c94f643d4488c20acdc9c4
cyr <bochkarev.cyril@gmail.com>
2022-08-28 14:14:58 +0200

    fix list get; progress on iterator

--- src/main/kotlin/org/jub/kotlin/hometask3/BremenAvlList.kt
+++ src/main/kotlin/org/jub/kotlin/hometask3/BremenAvlList.kt
@@ -1,6 +1,6 @@
 package org.jub.kotlin.hometask3
 
-import java.util.*
+import kotlin.ConcurrentModificationException
 
 class BremenAvlList<K : Comparable<K>, V>() : BremenAvlBase<K, V>(), AvlTreeList<K, V> {
     constructor(from: Collection<Pair<K, V>>) : this() {
@@ -14,22 +14,24 @@ class BremenAvlList<K : Comparable<K>, V>() : BremenAvlBase<K, V>(), AvlTreeList
     override fun containsAll(elements: Collection<V>): Boolean = elements.all { contains(it) }
 
     override fun get(index: Int): V {
+        require(index >= 0) { "Index must be positive" }
         if (root == null || index >= root?.size!!) {
             throw IndexOutOfBoundsException("Index $index is out of bounds")
         }
         var mutableIndex = index
         var curr = root
-        while (true) {
+        while (mutableIndex != 0) {
             val leftSize = curr?.left?.size ?: 0
             curr = when {
                 mutableIndex < leftSize -> curr?.left
                 mutableIndex == leftSize -> return curr!!.value
                 else -> {
-                    mutableIndex -= leftSize
+                    mutableIndex -= leftSize + 1
                     curr?.right
                 }
             }
         }
+        return curr!!.leftmost().value
     }
 
     override fun iterator(): Iterator<V> = listIterator()
@@ -37,23 +39,30 @@ class BremenAvlList<K : Comparable<K>, V>() : BremenAvlBase<K, V>(), AvlTreeList
     override fun listIterator(): ListIterator<V> = listIterator(0)
 
     override fun listIterator(index: Int): ListIterator<V> = object : ListIterator<V> {
-        private val pathFromRoot: Stack<BremenAvlNode<K, V>> = Stack<BremenAvlNode<K, V>>()
-        private var currIndex = 0
+        private var cursor = 0
+        private val modCount = this@BremenAvlList.modCount
+        private val source = this@BremenAvlList
 
-        init {
-            root?.let { pathFromRoot.push(it) }
+        private fun checkForComodification() {
+            if (this.modCount != source.modCount)
+                throw ConcurrentModificationException("Iterator is invalid since the original list was modified")
         }
 
         override fun hasNext(): Boolean {
-            TODO("Not yet implemented")
+            checkForComodification()
+            return source.size > 0 && cursor != source.size - 1
         }
 
         override fun hasPrevious(): Boolean {
-            TODO("Not yet implemented")
+            return cursor > 0
         }
 
         override fun next(): V {
-            TODO("Not yet implemented")
+            if (!hasNext())
+                throw ArrayIndexOutOfBoundsException("Cannot get index $cursor from the list")
+            val value = source[cursor]
+            cursor += 1
+            return value
         }
 
         override fun nextIndex(): Int {
@@ -80,4 +89,30 @@ class BremenAvlList<K : Comparable<K>, V>() : BremenAvlBase<K, V>(), AvlTreeList
     override fun indexOf(element: V): Int {
         TODO("Not yet implemented")
     }
+
+    // modifications
+    internal var modCount = 0
+
+
+    override fun clear() {
+        modCount += 1
+        super.clear()
+    }
+
+    override fun put(key: K, value: V): V? {
+        modCount += 1
+        return super.put(key, value)
+    }
+
+    override fun putAll(from: Map<out K, V>) {
+        modCount += 1
+        super.putAll(from)
+    }
+
+    override fun remove(key: K): V? {
+        modCount += 1
+        return super.remove(key)
+    }
+
+    // Too lazy to do putIfAbsent and so on
 }
