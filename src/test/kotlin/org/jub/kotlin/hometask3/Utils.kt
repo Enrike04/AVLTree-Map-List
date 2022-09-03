ffb9be5e86db0f67080aa8610314e866e5ed99a1
cyr <bochkarev.cyril@gmail.com>
2022-09-03 18:41:57 +0200

    remove solution

--- src/test/kotlin/org/jub/kotlin/hometask3/Utils.kt
+++ src/test/kotlin/org/jub/kotlin/hometask3/Utils.kt
@@ -6,5 +6,3 @@ internal const val TEST_ITERATIONS = 10
 internal const val TEST_SET_SIZE = 128
 
 internal fun getSetOfRandomValues(size: Int = TEST_SET_SIZE) = IntArray(size) { Random.nextInt() }.toSet()
-
-typealias AvlTreeImpl<K, V> = BremenAvlList<K, V>
