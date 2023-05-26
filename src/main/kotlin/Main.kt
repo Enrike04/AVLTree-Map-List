18b328feb91c85c1492c9dc5d8ce1a1e7c8ba3e4
Kirill Bochkarev <kirill.bochkarev@jetbrains.com>
2023-05-26 09:38:24 +0200

    remove solution

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,21 +1,8 @@
-import org.jub.kotlin.hometask3.getMutableAvlTreeMap
-import org.jub.kotlin.hometask3.solution.MutableAvlTreeMapImpl
+import org.jub.kotlin.hometask3.getAvlTree
 import kotlin.random.Random
 
 fun main() {
     val wow = List(10) { it to Random.nextInt(100) }
-    val avl = getMutableAvlTreeMap(wow)
-    println(avl.entries.size)
-    for (element in avl.entries) {
-        println(element)
-    }
-
-//    val map = mutableMapOf(1 to 2, 3 to 4)
-//    val entr = map.entries
-//    entr.forEach { print("$it; ") }
-//    println(".")
-//    entr.removeIf { it.key == 1 }
-//    entr.forEach { print("$it; ") }
-//    println(".")
-//    map.forEach { print("$it; ") }
+    val avl = getAvlTree(wow)
+    println(avl.maximumKey())
 }
--- src/main/kotlin/org/jub/kotlin/hometask3/HelperFunctions.kt
+++ src/main/kotlin/org/jub/kotlin/hometask3/HelperFunctions.kt
@@ -1,30 +1,9 @@
 package org.jub.kotlin.hometask3
 
-import org.jub.kotlin.hometask3.solution.AvlMutableMap
-import org.jub.kotlin.hometask3.solution.BaseAvl
+fun <K : Comparable<K>, V> getAvlTree(collection: Iterable<Pair<K, V>>): AvlTree<K, V> = TODO()
 
-fun <K : Comparable<K>, V> getAvlTree(collection: Iterable<Pair<K, V>>): AvlTree<K, V> {
-    val base = BaseAvl<K, V>()
-    collection.forEach { (k, v) ->
-        base.insert(k, v)
-    }
-    return base
-}
+fun <K : Comparable<K>, V> getAvlTreeMap(collection: Iterable<Pair<K, V>>): AvlTreeMap<K, V> = TODO()
 
-fun <K : Comparable<K>, V> getAvlTreeMap(collection: Iterable<Pair<K, V>>): AvlTreeMap<K, V> {
-    val avl = AvlMutableMap<K, V>()
-    collection.forEach { (k, v) ->
-        avl.insert(k, v)
-    }
-    return avl
-}
-
-fun <K : Comparable<K>, V> getMutableAvlTreeMap(collection: Iterable<Pair<K, V>>): MutableAvlTreeMap<K, V> {
-    val avl = AvlMutableMap<K, V>()
-    collection.forEach { (k, v) ->
-        avl.insert(k, v)
-    }
-    return avl
-}
+fun <K : Comparable<K>, V> getMutableAvlTreeMap(collection: Iterable<Pair<K, V>>): MutableAvlTreeMap<K, V> = TODO()
 
 fun <K : Comparable<K>, V> getAvlTreeList(collection: Iterable<Pair<K, V>>): AvlTreeList<K, V> = TODO()
